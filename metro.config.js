36e6241620173ce598b10551b30244e15917bdb6
Veldrine Evelia Kaharwa <eveliaveldrine@outlook.com>
2025-08-03 15:15:53 +0000

    update deps

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,14 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add resolver for crypto and other node modules
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    crypto: require.resolve('expo-crypto'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('@craftzdog/react-native-buffer').default,
+  }
+};
+
 module.exports = config;
